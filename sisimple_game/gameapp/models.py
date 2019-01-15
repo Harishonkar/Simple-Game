35eb5a3e77605f4e5e7e5de36e619744255854e5
Hareesh Hebballi <hareesh.hebballi@kavach.net>
2019-01-15 12:38:17 +0530

    Database table is created

--- sisimple_game/gameapp/models.py
+++ sisimple_game/gameapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Hero(models.Model):
+    status=models.CharField(max_length=100)
+    win_score=models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.id)
